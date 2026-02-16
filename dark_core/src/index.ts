fce76690b1d4fca1cd39de413ee5040865737638
Alex Carter <victoryforphil@gmail.com>
2026-02-15 21:46:51 -0800

    Core // Added Basic Elysia JS Setup

--- dark_core/src/index.ts
+++ dark_core/src/index.ts
@@ -1,19 +1,28 @@
 import { Elysia } from 'elysia';
 import { openapi } from '@elysiajs/openapi'
 import { logger } from '@grotto/logysia';
+import { llms } from "@opuu/elysia-llms-txt";
 
 import Log from './utils/logging';
 
 const app = new Elysia()
 .use(openapi()) 
+.use(
+    llms({
+      source: {
+        type: "url",
+        url: "/openapi/json",
+      },
+    })
+  )
 .use(logger({ 
             logIP: false,
             writer: {
                 write(msg: string) {
-                  logger.info(msg.trim())
+                  Log.info(`Core // HTTP // ${msg.trim()}`);
                 }
             }
         }))
-.get('/', () => 'Hello Elysia').listen(3000);
+.get('/', () => 'Hello Elysia').listen(4150);
 
-Log.info('Core // HTTP // Server is running on http://localhost:3000');
+Log.info('Core // HTTP // Server is running on http://localhost:4150');
